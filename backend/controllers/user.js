fb1e6ef38654c9e952e0bb847681e8bac107546c
Mohmed Ishak <ishakmohmed1911@gmail.com>
2021-05-14 01:52:42 +0800

    Initialized backend, added route for signup/auth.

---
+++ backend/controllers/user.js
@@ -0,0 +1,70 @@
+import asyncHandler from "express-async-handler";
+import generateToken from "../utils/generateToken.js";
+import User from "../models/user.js";
+
+const authenticateUser = asyncHandler(async (req, res) => {
+  const { email, password } = req.body;
+
+  const user = await User.findOne({ email });
+
+  if (user && (await user.matchPassword(password))) {
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+      token: generateToken(user._id),
+    });
+  } else {
+    res.status(401);
+    throw new Error("Invalid email or password.");
+  }
+});
+
+const registerUser = asyncHandler(async (req, res) => {
+  const { name, email, password } = req.body;
+
+  const user = await User.findOne({ email });
+
+  if (user) {
+    res.status(400);
+    throw new Error("User already exists.");
+  }
+
+  const createdUser = await User.create({
+    name,
+    email,
+    password,
+  });
+
+  if (createdUser) {
+    res.status(201).json({
+      _id: createdUser._id,
+      name: createdUser.name,
+      email: createdUser.email,
+      isAdmin: createdUser.isAdmin,
+      token: generateToken(createdUser._id),
+    });
+  } else {
+    res.status(400);
+    throw new Error("Invalid user data.");
+  }
+});
+
+const getUserProfile = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.user._id);
+
+  if (user) {
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+    });
+  } else {
+    res.status(404);
+    throw new Error("User not found");
+  }
+});
+
+export { authenticateUser, getUserProfile, registerUser };
