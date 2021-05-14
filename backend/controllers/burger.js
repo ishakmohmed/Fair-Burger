b0642fe0894ea442f6d8935735d057b4349ecebd
Mohmed Ishak <ishakmohmed1911@gmail.com>
2021-05-14 22:42:08 +0800

    Added controller for burger.

---
+++ backend/controllers/burger.js
@@ -0,0 +1,27 @@
+import asyncHandler from "express-async-handler";
+import Burger from "../models/burger.js";
+
+const addBurger = asyncHandler(async (req, res) => {
+  const { image, name, price } = req.body;
+
+  if (!image || !name || !price) {
+    res.status(400);
+    throw new Error(
+      "Bad request. Please make sure you enter name, price, and image of burger."
+    );
+  }
+
+  const createdBurger = await Burger.create({
+    image,
+    name,
+    price,
+  });
+
+  if (createdBurger) res.status(201);
+  else {
+    res.status(400);
+    throw new Error("Invalid burger data.");
+  }
+});
+
+export { addBurger };
--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -50,19 +50,4 @@ const registerUser = asyncHandler(async (req, res) => {
   }
 });
 
-const getUserProfile = asyncHandler(async (req, res) => {
-  const user = await User.findById(req.user._id);
-
-  if (user) {
-    res.json({
-      _id: user._id,
-      name: user.name,
-      email: user.email,
-    });
-  } else {
-    res.status(404);
-    throw new Error("User not found");
-  }
-});
-
-export { authenticateUser, getUserProfile, registerUser };
+export { authenticateUser, registerUser };
